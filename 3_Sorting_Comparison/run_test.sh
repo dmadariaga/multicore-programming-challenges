3813752acd037603153be95bf578e2e57b40ec58
Diego Madariaga <diego@niclabs.cl>
2017-07-01 16:31:04 -0400

    Fix sort test

--- 3_Sorting_Comparison/run_test.sh
+++ 3_Sorting_Comparison/run_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 outfile=results$(date +"%Y-%m-%d-%H:%M:%S").data
 
-echo "threads,time,n,alphabet,cache-misses" >> ${outfile}
+echo "algorithm,threads,n,alphabet,time,cache-misses" >> ${outfile}
 
 iterations=3
 for N in 10000 100000 1000000 10000000 # Reference size
@@ -14,7 +14,7 @@ do
             do
                 for j in 1 2 3 4 5 6 7 8 9 10 11 12 # iterate over cores
                 do
-        		    echo -n ${alg},${j}, >> ${outfile}
+        		    echo -n ${alg},${j},${N},${alphabet}, >> ${outfile}
         		    CILK_NWORKERS=${j} perf stat -o perf.tmp -x, -e cache-misses \
               		    ./${alg} ${N} ${alphabet} >> ${outfile}
         		    cut -d, -f1 perf.tmp | sed '/#/d' | sed '/^$/d' \
